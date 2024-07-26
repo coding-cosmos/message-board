ffde970d965102298a8e9eb645ec4cdf5db9c033
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-07-26 18:32:09 +0530

    Refactor index router to use messageController

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const messageController = require('../controllers/messageController');
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+router.get('/', messageController.messageListGet);
+router.get('/new',messageController.newMessageGet);
+router.post('/new',messageController.newMessagePost);
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Message',messages:messages});
-});
-
-router.get('/new',function(req,res,next){
-  res.render('form');
-})
-
-router.post('/new',function(req,res,next){
-  messages.push({text:req.body.message,user:req.body.name,added: new Date()});
-  res.redirect('/');
-});
 module.exports = router;
